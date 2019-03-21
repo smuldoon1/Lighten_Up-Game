0e9557e286e6a4ad06eeb9c9ff075448fa519a5e
seanmul10 <36199832+seanmul10@users.noreply.github.com>
2019-03-21 02:50:45 +0000

    Win condition scripts

---
+++ WinConditions.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinConditions : MonoBehaviour
+{
+    public enum CompletionAmount { No_Condition, Percentage, All }
+    public enum Player { No_Condition, Human_Win, Candle_Win }
+
+    public CompletionAmount tasksTillCompletion;
+    public CompletionAmount burnTillCompletion;
+    public Player timeOutWinner;
+
+    public float taskCompletion;
+    public float burnCompletion;
+    public int timeCompletion;
+
+    bool gameEnded = false;
+    HouseHealth house;
+    float time = 0;
+
+    private void Start()
+    {
+        if (FindObjectOfType<HouseHealth>() != null)
+        {
+            house = FindObjectOfType<HouseHealth>();
+        }
+        else
+        {
+            Debug.LogWarning("No object of type " + typeof(HouseHealth) + " could be found.");
+        }
+        if (burnTillCompletion == CompletionAmount.All)
+        {
+            burnCompletion = 100;
+        }
+        if (tasksTillCompletion == CompletionAmount.All)
+        {
+            taskCompletion = 100;
+        }
+    }
+
+    private void Update()
+    {
+        if (!gameEnded)
+        {
+            if (burnTillCompletion != CompletionAmount.No_Condition && house != null)
+            {
+                if (1 - (house.currentHealth / house.totalHealth) >= burnCompletion)
+                {
+                    PlayerWin("Candle");
+                }
+            }
+
+            if (tasksTillCompletion != CompletionAmount.No_Condition)
+            {
+                //Code for when tasks are done goes here
+                //PlayerWin("Human");
+            }
+
+            if (timeOutWinner != Player.No_Condition)
+            {
+                if (time >= timeCompletion)
+                {
+                    if (timeOutWinner == Player.Human_Win)
+                    {
+                        PlayerWin("Human");
+                    }
+                    else
+                    {
+                        PlayerWin("Candle");
+                    }
+                }
+                else
+                {
+                    time += Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    void PlayerWin(string player)
+    {
+        //Perform any code for when a player wins here
+        gameEnded = true;
+        Debug.Log(player + " wins!");
+    }
+}
---
+++ WinConditionsEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WinConditions))]
+public class WinConditionsEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        WinConditions wc = (WinConditions)target;
+
+        wc.burnTillCompletion = (WinConditions.CompletionAmount)EditorGUILayout.EnumPopup("BurnTillCompletion", wc.burnTillCompletion);
+        if (wc.burnTillCompletion == WinConditions.CompletionAmount.Percentage)
+        {
+            wc.burnCompletion = EditorGUILayout.Slider("CompletionPercentage", wc.burnCompletion, 0, 100);
+        }
+        EditorGUILayout.Space();
+
+        wc.tasksTillCompletion = (WinConditions.CompletionAmount)EditorGUILayout.EnumPopup("TasksTillCompletion", wc.tasksTillCompletion);
+        if (wc.tasksTillCompletion == WinConditions.CompletionAmount.Percentage)
+        {
+            wc.taskCompletion = EditorGUILayout.Slider("CompletionPercentage", wc.taskCompletion, 0, 100);
+        }
+        EditorGUILayout.Space();
+
+        wc.timeOutWinner = (WinConditions.Player)EditorGUILayout.EnumPopup("WinnerOnTimeUp", wc.timeOutWinner);
+        if (wc.timeOutWinner != WinConditions.Player.No_Condition)
+        {
+            EditorGUILayout.BeginHorizontal();
+            wc.timeCompletion = EditorGUILayout.IntField("TimeTillCompletion", wc.timeCompletion);
+            if (wc.timeCompletion < 0)
+            {
+                wc.timeCompletion = 0;
+            }
+            EditorGUILayout.LabelField(Mathf.Floor(wc.timeCompletion / 60).ToString().PadLeft(2, '0') + ":" + (wc.timeCompletion % 60).ToString().PadLeft(2, '0'));
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
